8ab896e16e8785dc64bf9859234584977e58ce58
Ronnie123-123 <ronneyosaeofosu@gmail.com>
2026-05-31 16:39:07 -0700

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,38 @@
+# File: initials.py
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 1
+#
+# Name: Ronney Osae Ofosu
+# STUDENT ID: 2425400361
+# Email: ronneyosaeofosu@gmail.com
+# Grader: Augustus
+#
+# On my honor,Ronney Osae Ofosu , this programming
+# assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
 
+    print()
+
+    print("...ROO")
+
+    print()
+
+    print("...RRRRRRRRRRRR........OOOOOOOOOOOO........OOOOOOOOOOOO.....")
+    print("...RR........RR........OO........OO........OO........OO.....")
+    print("...RR........RR........OO........OO........OO........OO.....")
+    print("...RR........RR........OO........OO........OO........OO.....")
+    print("...RRRRRRRRRRRR........OO........OO........OO........OO.....")
+    print("...RR.RR...............OO........OO........OO........OO.....")
+    print("...RR...RR.............OO........OO........OO........OO.....")
+    print("...RR.....RR...........OO........OO........OO........OO.....")
+    print("...RR.......RR....*...OO........OO......OO........OO...*")
+    print("...RR........RR...*...OOOOOOOOOOOO......OOOOOOOOOOOO...*")
+
+    print()
 
 
 main()
